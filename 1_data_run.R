0ff888a5981ef025c1ea72b555bc2593b88e958a
Emily Nightingale <phpuenig@lshtm.ac.uk>
2019-10-11 13:45:43 +0100

    Update 1_data_run.R
    Load simulated dataset and rename to "input".

--- 1_data_run.R
+++ 1_data_run.R
@@ -71,6 +71,7 @@ nbOrd <- nbOrder(poly2adjmat(shp), maxlag = 7)
 
 # Load input data (month-aggregated case counts for each block)
 load("input_sim.RData")
+input <- input_sim
 # load("./KAMIS/Analysis data/archive/input_jan13dec18_1406.RData")
 
 # Run user-defined functions 
